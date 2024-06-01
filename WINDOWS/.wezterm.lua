179d3582c4394dda8954f167fe1863ce38a20e75
edvardeishen <37629956+edvardeishen@users.noreply.github.com>
2024-06-01 13:53:41 +0300

    Configs for Windows added

---
+++ WINDOWS/.wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+config.font_size = 10.5
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.initial_rows = 50
+config.initial_cols = 200
+
+config.window_close_confirmation = "NeverPrompt"
+config.audible_bell = "Disabled"
+
+config.enable_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
